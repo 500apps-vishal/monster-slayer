61982d379b691ffe63bd67fa004c8cd7eb6f3105
500apps-vishal <84908640+500apps-vishal@users.noreply.github.com>
2021-07-06 20:33:26 +0530

    intial commit

---
+++ conditional/v-for.js
@@ -0,0 +1,10 @@
+new Vue({
+    el:"#app",
+    data:{
+       ingrediants:['apple','mango','fruity','banana'],
+       persons:[
+           {name:'Vishl',lastname:'chindam',age:22},
+            {name:'Dummmy',lastname:'chindam',age:23}
+       ]
+    }
+});
