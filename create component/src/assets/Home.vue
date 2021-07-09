a4932391102a8429c9f40ac23aaf1867c244a668
500apps-vishal <84908640+500apps-vishal@users.noreply.github.com>
2021-07-09 19:54:25 +0530

    intial commit

---
+++ create component/src/assets/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+<h1> Status:{{status}}</h1>
+<button v-on:click='changeStatus()'> Change Status</button>
+    </div>
+</template>
+
+
+<script>
+   export default{
+     data:function(){
+       return{
+           status:'Update'
+       }
+     },
+       methods:{
+           changeStatus(){
+           this.status='Delete';
+           }
+     }
+ }
+</script>
