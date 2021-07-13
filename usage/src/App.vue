eb7d8e4ad7cae2c5434af8a0e14b288a835f1b76
500apps-vishal <84908640+500apps-vishal@users.noreply.github.com>
2021-07-13 20:39:45 +0530

    commit

---
+++ usage/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class = "col-x5-12">
+              <button @click="selectedComponent='favorate Food'">Done</button>
+            <button  @click="selectedComponent='favorate dress'">Preview</button>
+            <button  @click="selectedComponent='Ur Choice plz'">Next</button>
+            <hr>
+            <p>  {{selectedComponent}}</p>
+           <component :is="selectedComponent">
+             <p>No content</p>
+             </component> 
+            </div>
+            
+            
+              <!--<app-Quote>-->
+                
+               <!-- <h1 slot="title">{{title}}</h1> -->
+               <!--<p>Cool</p>-->
+              <!--  </app-Quote>-->
+              </div>
+    </div>
+</template>
+<script>
+
+import quote from './components/quote.vue';
+import autho from './components/autho.vue';
+import neww from './components/neww.vue';
+
+
+export default {
+  data:function(){
+    return{
+      title: 'Hello World',
+      selectedComponent:'page'
+    }
+  },
+    component:{
+      appQuote: quote,
+      appneww:neww,
+      appautho:autho
+    }
+}
+</script>
---
+++ usage/src/components/quote.vue
@@ -0,0 +1,55 @@
+<!--<template>
+    <div >
+       <slot></slot>     
+        </div>
+</template>
+<script>
+</script>
+<style scoped>
+div{
+    border:1px solid #ccc;
+    box-shadow: 1px 1px 2px black;
+    padding:10px;
+    margin:1px;
+    text-align: center;
+
+}
+p{
+    background-color: bisque;
+     font-size: 30px;
+}
+</style>-->
+////////////////////
+//using multiple slots
+<template>
+<div>
+<div class="title">
+    <slot name="title"></slot>
+    <span style="color:#ccc"><slot name="subTitle"/>Not Bad</span>
+</div>
+<hr>
+    <div>
+       <slot></slot>     
+        </div>
+</div>
+</template>
+<script>
+</script>
+<style scoped>
+div{
+    border:1px solid #ccc;
+    box-shadow: 1px 1px 2px black;
+    padding:10px;
+    margin:1px;
+    text-align: center;
+
+}
+p{
+    background-color: bisque;
+     font-size: 30px;
+}
+.title{
+    font-style: italic;
+}
+</style>
+
